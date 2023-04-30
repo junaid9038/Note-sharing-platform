f1834669ac4648d3fd98d3822baa1aac5d3e1a20
Junaid Ahmed <junaidahmed9038@gmail.com>
2023-04-30 12:31:32 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use('/',require('./routes/index'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 
 app.listen(8000,function(err){
     if(err){
