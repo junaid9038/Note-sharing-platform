91bba945b9f3ef96a2c41353632d23d0e58d0f46
Junaid Ahmed <junaidahmed9038@gmail.com>
2023-05-01 14:35:24 +0530

    completed manual authentication

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+// require library and call for use layouts
+const expressEjsLayouts = require('express-ejs-layouts');
+app.use(expressEjsLayouts);
+
 app.use('/',require('./routes/index'));
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
 
 app.set('view engine','ejs');
 app.set('views','./views');
+// access for public folder
+app.use(express.static('./assets'));
 
 
 app.listen(8000,function(err){
