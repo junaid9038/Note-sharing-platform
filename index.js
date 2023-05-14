8a2e49712bf18130462ad3eb7080a1b100a15d7e
Junaid Ahmed <junaidahmed9038@gmail.com>
2023-05-14 12:16:59 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 
-// require library and call for use layouts
-const expressEjsLayouts = require('express-ejs-layouts');
+const expressEjsLayouts = require('express-ejs-layouts'); // require library and call for use layouts
+require('./config/mongoose');
+// const passport = require('passport');
+require('./config/passport-local-strategy');
+const expressSession = require('express-session');
+const passport = require('passport');
+const mongoStore = require('connect-mongo');
+
+app.use(express.urlencoded());// help to extract the data form body
 app.use(expressEjsLayouts);
+app.use(expressSession({
+    name:'sahinotes',
+    secret:'sahinotes_dev',
+    cookie:{
+        maxAge:24*60*60*1000
+    },
+    store:mongoStore.create({
+        mongoUrl:"mongodb://127.0.0.1:27017/sahinotes_devlopment"
+    })
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
 
 app.use('/',require('./routes/index'));
 
@@ -12,10 +32,14 @@ app.set('layout extractScripts',true);
 
 
 
+
 app.set('view engine','ejs');
 app.set('views','./views');
 // access for public folder
 app.use(express.static('./assets'));
+
+
+
 
 
 app.listen(8000,function(err){
